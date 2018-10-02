de52ca14f99ab1b855ee44f8e1b296185dd77ec8
rachel <goldpiggy@berkeley.edu>
2018-10-02 13:35:02 -0700

    minor changes

--- hw3/run_dqn_atari.py
+++ hw3/run_dqn_atari.py
@@ -33,7 +33,7 @@ exploration_schedule = PiecewiseSchedule(
         [
             (0, 1.0),
             (1e6, 0.1),
-            (num_iterations / 2, 0.01),
+            (1e8 / 2, 0.01),
         ], outside_value=0.01
     )
 
